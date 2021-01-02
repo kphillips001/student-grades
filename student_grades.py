03c32e9e128180a70572576dcb216dc064c5eb92
Kevin <knphillips01@gmail.com>
2021-01-01 18:52:44 -0600

    project done

---
+++ student_grades.py
@@ -0,0 +1,26 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+# Create an empty dictionary 
+student_grades = {}
+
+# Add the grades to student_grades.👇
+for student in student_scores:
+  score = student_scores[student]
+  if score > 90:
+    student_grades[student] = "Outstanding"
+  elif score > 80:
+    student_grades[student] = "Exceeds Expectations"
+  elif score > 70:
+    student_grades[student] = "Acceptable"
+  else:
+    student_grades[student] = "Fail"
+    
+
+# Print Grades
+print(student_grades)
